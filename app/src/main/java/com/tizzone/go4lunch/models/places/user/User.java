621436dc9a03e2db9720e5f8a725ca5dd9924969
docteurmabuse <laurent.tizzone@gmail.com>
2021-01-02 18:38:59 +0100

    - create User class with getter and setter

---
+++ app/src/main/java/com/tizzone/go4lunch/models/places/user/User.java
@@ -0,0 +1,74 @@
+package com.tizzone.go4lunch.models.places.user;
+
+import com.google.maps.android.data.LineString;
+
+import java.lang.reflect.Array;
+
+public class User {
+
+    private int id;
+    private String lastName;
+    private String photoUrl;
+    private Array favoriteRestaurants;
+    private int lunchSpot;
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public User setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+        return this;
+    }
+
+    public Array getFavoriteRestaurants() {
+        return favoriteRestaurants;
+    }
+
+    public User setFavoriteRestaurants(Array favoriteRestaurants) {
+        this.favoriteRestaurants = favoriteRestaurants;
+        return this;
+    }
+
+    public int getLunchSpot() {
+        return lunchSpot;
+    }
+
+    public User setLunchSpot(int lunchSpot) {
+        this.lunchSpot = lunchSpot;
+        return this;
+    }
+
+
+
+}
