857aaf67960cccb9d5bbbb47c861f8cb1ada68d3
docteurmabuse <laurent.tizzone@gmail.com>
2021-02-18 20:40:48 +0100

    - add cache in NetworkModule

--- app/src/main/java/com/tizzone/go4lunch/di/NetworkModule.java
+++ app/src/main/java/com/tizzone/go4lunch/di/NetworkModule.java
@@ -2,28 +2,124 @@ package com.tizzone.go4lunch.di;
 
 import com.tizzone.go4lunch.network.PlacesApiService;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.ActivityRetainedComponent;
-import dagger.hilt.android.scopes.ActivityRetainedScoped;
+import dagger.hilt.components.SingletonComponent;
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.facebook.FacebookSdk.getCacheDir;
+
 @Module
-@InstallIn(ActivityRetainedComponent.class)
+@InstallIn(SingletonComponent.class)
 public class NetworkModule {
 
     @Provides
-    @ActivityRetainedScoped
-    public PlacesApiService provideGooglePlacesApiService() {
+    @Singleton
+    public PlacesApiService provideGooglePlacesApiService(OkHttpClient httpClient) {
         return new Retrofit.Builder()
                 .baseUrl("https://maps.googleapis.com/maps/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .client(httpClient)
                 .build()
                 .create(PlacesApiService.class);
     }
 
+    @Provides
+    public HttpLoggingInterceptor provideHttpLoggingInterceptor() {
+        return new HttpLoggingInterceptor();
+    }
+
+    @Provides
+    public OkHttpClient provideHttpClient() {
+        // HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        //httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        File httpCacheDirectory = new File(getCacheDir(), "offlineCache");
+
+        //10 MB
+        Cache cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(provideCacheInterceptor())
+                .addNetworkInterceptor(provideCacheInterceptor())
+                .addInterceptor(provideOfflineCacheInterceptor())
+                .build();
+    }
+
+    @Provides
+    public Interceptor provideOfflineCacheInterceptor() {
+
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                try {
+                    return chain.proceed(chain.request());
+                } catch (Exception e) {
+
+
+                    CacheControl cacheControl = new CacheControl.Builder()
+                            .onlyIfCached()
+                            .maxStale(1, TimeUnit.DAYS)
+                            .build();
+
+                    Request offlineRequest = chain.request().newBuilder()
+                            .cacheControl(cacheControl)
+                            .build();
+                    return chain.proceed(offlineRequest);
+                }
+            }
+        };
+    }
+
+    @Provides
+    public Interceptor provideCacheInterceptor() {
+
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                Response originalResponse = chain.proceed(request);
+                String cacheControl = originalResponse.header("Cache-Control");
+
+                if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                        cacheControl.contains("must-revalidate") || cacheControl.contains("max-stale=0")) {
+
+
+                    CacheControl cc = new CacheControl.Builder()
+                            .maxStale(1, TimeUnit.DAYS)
+                            .build();
+
+
+                    request = request.newBuilder()
+                            .cacheControl(cc)
+                            .build();
+
+                    return chain.proceed(request);
+
+                } else {
+                    return originalResponse;
+                }
+            }
+        };
+
+    }
+
 }
+
