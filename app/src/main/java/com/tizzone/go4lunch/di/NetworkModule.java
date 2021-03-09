5b841870eb0d94bcc2897207ad53efd799cf7335
laurent.tizzone@gmail.com <JsnaPdl75011_!>
2021-03-09 20:29:50 +0100

    - delete pref_main.xml

--- app/src/main/java/com/tizzone/go4lunch/di/NetworkModule.java
+++ app/src/main/java/com/tizzone/go4lunch/di/NetworkModule.java
@@ -2,9 +2,6 @@ package com.tizzone.go4lunch.di;
 
 import com.tizzone.go4lunch.network.PlacesApiService;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -12,91 +9,30 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
-import okhttp3.Cache;
-import okhttp3.CacheControl;
 import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import static com.facebook.FacebookSdk.getCacheDir;
 
 @Module
 @InstallIn(SingletonComponent.class)
 public class NetworkModule {
     @Provides
     @Singleton
-    public PlacesApiService provideGooglePlacesApiService(OkHttpClient httpClient) {
+    public PlacesApiService provideGooglePlacesApiService() {
         HttpUrl baseUrl = HttpUrl.get("https://maps.googleapis.com/maps/api/");
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .client(httpClient)
                 .build()
                 .create(PlacesApiService.class);
     }
 
     @Provides
-    public HttpLoggingInterceptor provideHttpLoggingInterceptor() {
-        return new HttpLoggingInterceptor();
-    }
-
-    @Provides
     public OkHttpClient provideHttpClient() {
-        File httpCacheDirectory = new File(getCacheDir(), "offlineCache");
-        //10 MB
-        Cache cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
         return new OkHttpClient.Builder()
-                .cache(cache)
-                .addInterceptor(provideCacheInterceptor())
-                .addNetworkInterceptor(provideCacheInterceptor())
-                .addInterceptor(provideOfflineCacheInterceptor())
                 .build();
-    }
-
-    @Provides
-    public Interceptor provideOfflineCacheInterceptor() {
-        return chain -> {
-            try {
-                return chain.proceed(chain.request());
-            } catch (Exception e) {
-                CacheControl cacheControl = new CacheControl.Builder()
-                        .onlyIfCached()
-                        .maxStale(1, TimeUnit.DAYS)
-                        .build();
-                Request offlineRequest = chain.request().newBuilder()
-                        .cacheControl(cacheControl)
-                        .build();
-                return chain.proceed(offlineRequest);
-            }
-        };
-    }
-
-    @Provides
-    public Interceptor provideCacheInterceptor() {
-        return chain -> {
-            Request request = chain.request();
-            Response originalResponse = chain.proceed(request);
-            String cacheControl = originalResponse.header("Cache-Control");
-            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
-                    cacheControl.contains("must-revalidate") || cacheControl.contains("max-stale=0")) {
-                CacheControl cc = new CacheControl.Builder()
-                        .maxStale(1, TimeUnit.DAYS)
-                        .build();
-                request = request.newBuilder()
-                        .cacheControl(cc)
-                        .build();
-                return chain.proceed(request);
-
-            } else {
-                return originalResponse;
-            }
-        };
     }
 }
 
